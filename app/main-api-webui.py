c3543328a2cf5cf3ab997acdfd7f22d4ebb37fff
chenshuai <chenshuai@amberweather.com>
2024-08-27 10:47:22 +0800

    add models

--- app/main-api-webui.py
+++ app/main-api-webui.py
@@ -21,11 +21,13 @@ supported_openai_models = [
     "gpt-3.5-turbo",
     "gpt-4-turbo",
     "gpt-4o",
+    "gpt-4o-mini",
     "gemini-1.5-flash",
     "gemini-1.5-pro",
     "claude-3-haiku-20240307",
     "claude-3-sonnet-20240229",
     "claude-3-opus-20240229",
+    "claude-3-5-sonnet-20240620"
 ]
 
 
